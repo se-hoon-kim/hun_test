defd94899ed4eb7884ffe1b2741df5230f44c0a8
hun3 <hun3@DESKTOP-TF72DBH>
2016-11-12 23:22:18 +0900

    커밋

---
+++ src/main/java/com/my/h3/HomeController.java
@@ -0,0 +1,60 @@
+package com.my.h3;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+
+	@RequestMapping("/helloWorld")
+	public ModelAndView helloWorld() {
+		ModelAndView mav = new ModelAndView();
+		System.out.println("===================");
+		mav.setViewName("/helloWorld");
+		return mav;
+	}
+
+	@RequestMapping("/ajax")
+	public Map get() {
+		System.out.println("===get");
+		Map map = new HashMap();
+		map.put("key", "value");
+		return map;
+	}
+
+	
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+<h1>
+	Hello world!  
+</h1>
+
+<P>  The time on the server is ${serverTime}. </P>
+
+이것이 스프링이다.  ㅋㅋㅋㅋ
+
+my_b
+</body>
+</html>
